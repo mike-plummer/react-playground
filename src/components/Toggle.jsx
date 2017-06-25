be82ee81664dcd6814f7f4be26ff7726ad419e5b
Mike Plummer <mike-plummer@users.noreply.github.com>
2017-06-25 12:47:54 -0500

    Styling

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 export class Toggle extends React.Component {
 
     render() {
-        return <div onClick={() => this.props.onClick()}>{this.props.value}</div>
+        return <div
+            onClick={() => this.props.onClick()}
+            className={'toggle toggle'+(this.props.value % 4)}>
+              {this.props.value}
+          </div>
     }
 }
